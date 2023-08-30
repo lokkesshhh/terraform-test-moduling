28559dc15e80b5b685dbee242972cde79fef2b41
Lokkesshhh <lokeshdutt1992@gmail.com>
2023-08-30 19:21:28 -0400

    add new files

--- main.tf
+++ main.tf
@@ -46,18 +46,18 @@
 #   # config
 
 # }
-# terraform {
-#   required_version = ">= 1.0.0"
-#   required_providers {
-#     azurerm = {
-#       source = "hashicorp/azurerm"
-#       version = ">= 2.0" 
-#     }    
-#   }
-# }
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
+}
 
 # Provider Block
 provider "azurerm" {
- features {}          
+  features {}
 }
 
